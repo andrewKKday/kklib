ca6f0462e8605a3203578577d78b42c272127255
陳冠廷 <andrew.chen@kkday.com>
2024-08-14 17:07:25 +0800

    feat: add SQL comment

--- sql/V1__Initial.sql
+++ sql/V1__Initial.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS public.lib_user;
 DROP TABLE IF EXISTS public.book;
 CREATE  TABLE  public.lib_user (
         user_oid SERIAL PRIMARY KEY NOT NULL,   -- SERIAL == conti int
-        create_user varchar(32) NULL,   -- always need
+        create_user varchar(32) NULL,   -- always need (extends baseEntity)
         modify_user varchar(32) NULL,   -- always need
         create_date timestamp NULL, -- always need
         modify_date timestamp NULL, -- always need
